f8c32e1904b1ca01444f8f04296aaa049b0ed8a9
Jonas Hölzel <jonashoelzel22@gmail.com>
2022-05-30 17:43:01 +0200

    Fix Login

--- lib/Util/register_new_user.dart
+++ lib/Util/register_new_user.dart
@@ -1,13 +1,36 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class RegisterNewUser {
   static Future<bool> tryRegisterEMail(String email) async {
+    if (!FirebaseAuth.instance.isSignInWithEmailLink("allusers@example.com")) {
+      if (!await _login()) {
+        return false;
+      }
+    }
+
     try {
       final db = FirebaseFirestore.instance.collection("first-challenge-email");
       await db.add({"email": email});
       return true;
+    } catch (e) {
+      return false;
     }
-    catch(e) {
+  }
+
+  static Future<bool> _login() async {
+    try {
+      final userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(
+              email: "allusers@example.com", password: "lösung");
+
+      return userCredential.user != null;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
       return false;
     }
   }
